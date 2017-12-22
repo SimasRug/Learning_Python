4ff0f7c9f35d46af99dfe36a1d94e73f251611cb
Simonas Rugevicius <Rugeviciuss@petrolink.net>
2017-12-22 16:32:07 +0100

    remaking connect4

--- Chapter8/connect_4.py
+++ Chapter8/connect_4.py
@@ -1,3 +1,9 @@
+import random
+from random import randint
+import copy
+from copy import deepcopy
+
+
 width, height = 7, 6
 game_board = [[0 for x in range(width)] for y in range(height)]
 values = {0: ' ', 1: 'R', 2: 'Y'}
@@ -11,47 +17,64 @@ def print_board(board):
                 print()
 
 
-def check_available_postion(board):
+def check_available_positions(board):
     free_positions = []
-    for x in range(0, len(board)):
-        for j in range(0,len(board[x])):
-            if(board[x][j] == 0):
-                free_positions.append([x,j])
+    for x in range (0, len(board[0])):
+        if(board[0][x] == 0):
+            free_positions.append(x)
     return free_positions
+           
+
+
+
            
 
 def player_move(board, player):
     position = int(input('Enter position (1-7): '))
     if 0 < position and position < 8:
         position = position - 1
-        if(board[0][position] != 0):
+        available_col = check_available_positions(board)
+        if(position not in available_col):
             print('Position taken')
             return player_move(board, player)
         else:
             needed_comlumn = []
-              
-            available_positions = check_available_postion(board)
-
-            for x in available_positions:
-                if(x[1] == position):
-                    needed_comlumn.append(x)
-            
-            fall_move = -1
-            for x in needed_comlumn:
-                # print (x)
-                if(x[0] > fall_move):
-                    fall_move = x[0]
-
-            board[fall_move][position] = player
-            if(check_for_win(board, player, (fall_move, position))):
-                return True, game_board
+            test = False
+            incrament_me = 0
+            while not test:
                 
-            return False, board
+                
+        return False, board
+        #  if(board[0][position] != 0):
+        #     print('Position taken')
+        #     return player_move(board, player)
+        # else:
+        #     needed_comlumn = []
+              
+        #     available_positions = check_available_positions(board)
+
+        #     for x in available_positions:
+        #         if(x[1] == position):
+        #             needed_comlumn.append(x)
+            
+        #     fall_move = -1
+        #     for x in needed_comlumn:
+        #         if(x[0] > fall_move):
+        #             fall_move = x[0]
+
+        #     board[fall_move][position] = player
+
+        #     if(check_for_win(board, player)):
+        #         return True, game_board
+        #     elif(len(check_available_positions(board)) == 0):
+        #         print('Tie Game')
+        #         return True, game_board
+        #     return False, board
     else:
       raise ValueError ('Wrong input')
 
 
-def check_for_win(board, player, move):
+def check_for_win(board, player):
     # height 6 for arr +1 width 7 for arr +1
 
     for j in range(0, 6):
@@ -82,6 +105,48 @@ def check_for_win(board, player, move):
     return False
 
 
+def computer_move(board, player):
+    val, mov = minimax(board, player)
+    print(val, mov)
+    # board[mov[0]][mov[1]] = player
+    # if(check_for_win(board, player)):
+    #     return True, game_board
+    # elif(len(check_available_positions(board)) == 0):
+    #     print('Tie Game')
+    #     return True, game_board    
+    return False, board
+
+
+def minimax(board, player):
+    available_positions = check_available_positions(board)
+    # available_positions.reverse()
+
+    if(check_for_win(board, 1)):
+        return (-10, [-1,-1])
+    elif(check_for_win(board,2)):
+        return (10, [-1,-1])
+    elif(available_positions == []):
+        return (0, [-1,-1])
+
+    if(player == 2):
+        best_value = -1000
+        best_move = [-1,-1]
+        print(player, best_move, best_value)
+        for x in available_positions:
+            new_board = copy.deepcopy(board)
+            new_board[x[0]][x[1]] = player
+            print(new_board, '\n')
+            value, move = minimax(new_board, 2)
+
+            if(value > best_value):
+                best_value = value
+                best_move = [x[0],x[1]]
+                print([x[0],x[1]])
+        return best_value, best_move
+    
+
+
+    # return (0,[-1,-1])
 
 
 game_over = False
@@ -94,10 +159,10 @@ while not game_over:
             print(err_msg,'\n')
         print_board(game_board)
 
-    if not game_over:
-        try:
-            game_over, game_board = player_move(game_board, 2)
-        except ValueError as err_msg:
-            print(err_msg,'\n')
+    # if not game_over:
+    #     try:
+    #         game_over, game_board = computer_move(game_board, 2)
+    #     except ValueError as err_msg:
+    #         print(err_msg,'\n')
 
-        print_board(game_board)
+        # print_board(game_board)
